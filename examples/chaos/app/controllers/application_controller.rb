2099401162153f05ff2a27a11f7cf385f2388132
gcohen <geremy@pubnub.com>
2014-05-29 20:31:39 -0700

    Added passenger. Added CORS headers support. WIP on subscribe controller. Removed HTML layout.

--- examples/chaos/app/controllers/application_controller.rb
+++ examples/chaos/app/controllers/application_controller.rb
@@ -7,11 +7,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def set_pn_vars
+
+    response.headers['Access-Control-Allow-Origin:'] = '*'
+    response.headers['Access-Control-Allow-Methods:'] = 'GET'
+
     @pub_key = params[:pub_key]
     @sub_key = params[:sub_key]
     @jsonp = params[:jsonp]
-    @timetoken = params[:timetoken]
+    @timetoken = params[:timetoken].to_i
     @channels = params[:channels].split(",")
+
+
 
   end
 
--- examples/chaos/app/controllers/subscriber_controller.rb
+++ examples/chaos/app/controllers/subscriber_controller.rb
@@ -2,5 +2,19 @@ class SubscriberController < ApplicationController
 
   def subscribe
     @channels
+
+    if @timetoken == 0
+      render :js => make_msg(0)
+    end
   end
+
+
+  def make_msg(timetoken, messages = [])
+    if timetoken == 0
+      timetoken = (Time.now.to_i * 10000000).to_s
+    end
+    msg = [ messages, timetoken.to_s].to_json
+  end
+
+
 end
